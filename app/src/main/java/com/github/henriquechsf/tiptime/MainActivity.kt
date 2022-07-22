aed541e1d2c4ebe9c9608af7fb998a87cae4f5f5
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-07-21 23:21:35 -0300

    fix bugs

--- app/src/main/java/com/github/henriquechsf/tiptime/MainActivity.kt
+++ app/src/main/java/com/github/henriquechsf/tiptime/MainActivity.kt
@@ -3,6 +3,7 @@ package com.github.henriquechsf.tiptime
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.github.henriquechsf.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +13,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        if (cost == null) {
+            binding.tipResult.text = ""
+            return
+        }
+
+        var tip = tipPercentage * cost
+        val roundUp = binding.roundUpSwitch.isChecked
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
