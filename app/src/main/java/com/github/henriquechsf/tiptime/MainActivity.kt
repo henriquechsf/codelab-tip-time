96e782e82e4da9f1d8884b8ae9e27a6e2957b333
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-07-23 17:44:26 -0300

    hide keyboard on press enter

--- app/src/main/java/com/github/henriquechsf/tiptime/MainActivity.kt
+++ app/src/main/java/com/github/henriquechsf/tiptime/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.github.henriquechsf.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.github.henriquechsf.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -15,6 +19,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculateButton.setOnClickListener { calculateTip() }
+
+        binding.costOfServiceEditText.setOnKeyListener { v, keyCode, _ -> handleKeyEvent(v, keyCode) }
     }
 
     private fun calculateTip() {
@@ -43,5 +49,15 @@ class MainActivity : AppCompatActivity() {
     private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide keyboard
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
